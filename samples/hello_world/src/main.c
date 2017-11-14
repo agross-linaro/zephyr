b454efa4104d5183c5d8fc38058fc26fb8a9c7df
Andy Gross <andy.gross@linaro.org>
2017-11-14 12:31:00 -0600

    test code
    Signed-off-by: Andy Gross <andy.gross@linaro.org>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,7 +7,30 @@
 #include <zephyr.h>
 #include <misc/printk.h>
 
+static K_THREAD_STACK_DEFINE(stack0, 1024);
+static K_THREAD_STACK_DEFINE(stack1, 1024);
+static struct k_thread __kernel thread0;
+static struct k_thread __kernel thread1;
+
+void print_stuff(void *id, void * unused1, void *unused2)
+{
+	ARG_UNUSED(id);
+	ARG_UNUSED(unused1);
+	ARG_UNUSED(unused2);
+
+	while (1) {
+		printk("%d: foo\n", (u32_t) id);
+		k_sleep(1000);
+	}
+}
+
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_ARCH);
+	k_thread_create(&thread0, stack0, 1024, print_stuff, (void *)0,
+			NULL, NULL, 5, K_USER, K_FOREVER);
+	k_thread_create(&thread1, stack1, 1024, print_stuff, (void *)1,
+			NULL, NULL, 5, K_USER, K_FOREVER);
+
+	k_thread_start(&thread0);
+	k_thread_start(&thread1);
 }
